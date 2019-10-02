ad26f8a720fb1dac694b306bdb8508db75742ae7
MattMoony <monschein.matthias@gmail.com>
2019-10-02 22:26:29 +0200

    connected database

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-const express = require('express');
+const express = require('express'),
+      mysql = require('mysql');
 const app = express(),
-      conf = require('./conf.json');
+      conf = require('./conf.json'),
+      db_con = mysql.createConnection({
+          host: conf.db.host,
+          user: conf.db.user,
+          password: conf.db.pass,
+          database: conf.db.database,
+      });
 
-app.use('/', express.static('public/'));
-app.use('/', (req, res, next) => {
-    res.redirect('/404.html');
+db_con.connect(err => {
+    if (err) throw err;
+    console.log(`[MYSQL-DATABASE]:\tConnected as ${conf.db.user}@${conf.db.host}`);
+
+    app.use('/', express.static('public/'));
+
+    app.get('/api/get-task/:id', (req, res) => {
+        db_con.query(`SELECT title, description, solution
+                      FROM tasks
+                      WHERE id = ?;`, [+req.params.id], (err, result, fields) => {
+            if (err) res.status(500).send('Error 500: Internal server error!');
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(result));
+        });
+    });
+
+    app.use('/', (req, res, next) => {
+        res.redirect('/404.html');
+    });
+
+    app.listen(conf.port, () => console.log(`[WEB-SERVER]:\t\tListening on :${conf.port} ... `));
 });
-
-app.listen(conf.port, () => console.log(`[WEB-SERVER]: Listening on :${conf.port} ... `));
